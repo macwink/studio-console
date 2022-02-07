4923f77a3bf07cbc0f3d3a72f548f0af25c96c0e
Wingky Faisal <wingky.faisal@outlook.com>
2022-02-07 08:46:04 +0100

    using authenticator at the main App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Heading } from "@aws-amplify/ui-react";
 
-function App() {
+import { Amplify } from "aws-amplify";
+
+import { Authenticator, Card } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <div className="App">
-      <Heading>Hello World</Heading>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
